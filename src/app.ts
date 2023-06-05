9b64fac1e08ddefdda153e77b05c94fb10aef6fb
rifatjahan823 <rifatjahan823@gmail.com>
2023-06-05 22:05:57 +0600

    create user service

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
-import express, { Response, Request, urlencoded, Application } from 'express'
-import cors from 'cors'
+
+import express, { Response, Request, urlencoded, Application } from 'express';
+import cors from 'cors';
+import router from './app/modules/users/user.route';
+import { createUser } from './app/modules/users/user.service'
+
 
 const app: Application = express()
 
@@ -7,7 +11,14 @@ app.use(cors())
 app.use(express.json())
 app.use(urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+//application route
+app.use('/api/v1/users',router)
+
+
+
+//testing
+app.get('/', async(req: Request, res: Response) => {
+  createUser({id:'999',role:"student",password:'123'})
   res.send('Hello World!')
 })
 
