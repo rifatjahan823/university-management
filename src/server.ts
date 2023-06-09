e4863e6ed4aa7226e724a72a60b0f4b4f3bac930
rifatjahan823 <rifatjahan823@gmail.com>
2023-06-09 12:02:39 +0600

    setuplogger

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,19 @@
 import mongoose from "mongoose";
 import app from "./app";
 import config from "./config";
+import {errorLogger, logger }from "./shared/logger";
 
 
 async function main() {
   
 try{
     await mongoose.connect(config.database_url as string);
-    console.log(`😍 Databse connected successfully`)
+    logger.info(`😍 Databse connected successfully`)
     app.listen(config.port, () => {
-        console.log(`Application listening on port ${config.port}`)
+        logger.info(`Application listening on port ${config.port}`)
       })
 }catch(err){
-    console.log('Fail to connect database 😢',err)
+    errorLogger.error('Fail to connect database 😢',err)
 }
  
 }
